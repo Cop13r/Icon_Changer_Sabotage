97284b6fa13140595ba4d7cab50769230c03435e
COP <>
2024-03-01 14:27:27 -0500

    part 3
    ughhh

--- START.bat
+++ START.bat
@@ -2,12 +2,15 @@
 
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted"
 
-# Set System Variables
+# Set System Variables, problem,starts in system32 use dir
 
 powershell -Command "$env:MAIN = Get-Location; setx Main $env:MAIN /M"
 
+
+
 #Run powershell script that makes task schedule, run
 
+%MAIN%/
 
 
 #Copy files and folders
